92c03a4134f1c70607abe2649845d98d3bfa8e3a
Raúl Montón Pinillos <raul_m_p@me.com>
2024-03-31 15:46:28 +0200

    Added GitHub Pages docs

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
             targets: ["PowerMetricsKit"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "SampleThreads",
